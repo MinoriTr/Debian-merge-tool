2a94b0e5afe391f29cdcbd08706f3879294e34a6
TristanM <146242259+MinoriTr@users.noreply.github.com>
2026-03-30 19:08:54 +0200

    Update merge_script.sh

--- merge_script.sh
+++ merge_script.sh
@@ -42,9 +42,9 @@ if [[ $choix_dl_ISOs =~ ^[Oo][Uu][Ii]$ ]]; then
     echo -e "${yellow}DEUXIEME ETAPE : TELECHARGEMENT DES FICHIERS JIGDO ${defaut}"
     mkdir -p Debian-merge && cd Debian-merge || exit 1
 
-    for i in {1..6}; do
-        wget "https://cdimage.debian.org/debian-cd/current/amd64/jigdo-bd/debian-13.4.0-amd64-BD-${i}.jigdo" \
-         && wget "https://cdimage.debian.org/debian-cd/current/amd64/jigdo-bd/debian-13.4.0-amd64-BD-${i}.template"
+    for i in {1..27}; do
+        wget "https://cdimage.debian.org/debian-cd/current/amd64/jigdo-dvd-${i}.jigdo" \
+         && wget "https://cdimage.debian.org/debian-cd/current/amd64/jigdo-dvd-${i}.template"
     done
 
     echo -e "${yellow}TROISIEME ETAPE : CREATION DES ISOs AVEC JIGDO ${defaut}"
@@ -60,21 +60,20 @@ fi
 
 #Renommage des ISOs
 echo -e "${yellow}Renommage des ISOs a cause de l'alpha-numérique${defaut}"
-for file in debian-13.4.0-amd64-BD-*.iso; do
+for file in https://cdimage.debian.org/debian-cd/current/amd64/jigdo-dvd/debian-13.4.0-amd64-DVD-*.iso; do
     n=$(echo "$file" | grep -oP '(?<=DVD-)[0-9]+') 
-    mv "$file" $(printf "debian-13.4.0-amd64-BD-%02d.iso" "$n")
+    mv "$file" $(printf "debian-13.4.0-amd64-DVD-%02d.iso" "$n")
 done
-
+echo -e "${blue}ISOs crées. ${defaut}"
 
 # Fusion des ISOs
 echo -e "${yellow}QUATRIEME ETAPE : FUSION DES DVD${defaut}"
-echo -e "${blue}ISOs crées. ${defaut}"
 read -rp "Voulez-vous merger tous les ISOs ? (oui/non) : " choix_merge
 
 if [[ $choix_merge =~ ^[Oo][Uu][Ii]$ ]]; then
     chmod u+x ../merge_debian_isos
-    sh ../merge_debian_isos debian-13.4.0-amd64-merged.iso merge_mount/iso debian-13.4.0-amd64-BD-*.iso
-    sha256sum ./debian-13.4.0-amd64-BD-all.iso
+    sh ./merge_debian_isos debian-13.4.0-amd64-merged.iso merge_mount/iso ./debian-13.4.0-amd64-DVD-*.iso
+    sha256sum ./debian-13.4.0-amd64-merged.iso
     echo -e "${green}Checksum crée${defaut}"
     #chmod u+x ../debmerge.sh
     #bash ../debmerge.sh
